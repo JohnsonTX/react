0574391796c4167aa5928d9dba69c5ee12ce4e4e
johnsontx <johnsontx@johnsontxdeMacBook-Air.local>
2017-01-05 15:37:10 +0800

    the second submit

--- src/components/ReactwpApp.js
+++ src/components/ReactwpApp.js
@@ -7,29 +7,29 @@ var React = require('react/addons');
 require('normalize.css');
 require('../styles/main.css');
 //获取图片信息
-var imageDatas = require('../data/imagesData.json');
-    imageDatas = (function getImgData(imgArr){
-        for(var i = 0,j=imgArr.length;i<j;i++){
-            var singleImgData = imgArr[i];
-             singleImgData.imgURL = require('../images/' + singleImgData.fileName);
-            imgArr[i] = singleImgData;
+var imgData = require('../data/imagesData.json');
+    imgData = (function getImgURL(imgArr){
+        for(var i = 0,j = imgArr.length;i<j;i++){
+            var singleImg = imgArr[i];
+            singleImg.imgURL = require('../images/' + singleImg.fileName);
+            imgArr[i] = singleImg;
         }
         return imgArr;
-    })(imageDatas);
-function arranges(low,high){
-    return Math.ceil(Math.random() * (high - low) + low)
-};
+    })(imgData);
+ function posArannge(low,high){
+    return Math.ceil(Math.random() * (high - low) + low);
+ }
 var ImgFigure = React.createClass({
     render:function(){
         var styleObj = {};
         if(this.props.agData.pos){
             styleObj = this.props.agData.pos;
         }
-        return(
+        return (
             <div>
                 <figure className="img-figure" style={styleObj}>
                     <img src={this.props.data.imgURL} alt={this.props.data.title}/>
-                    <figcaption>
+                    <figcaption >
                         <h2 className="img-title">{this.props.data.title}</h2>
                     </figcaption>
                 </figure>
@@ -37,133 +37,144 @@ var ImgFigure = React.createClass({
         )
     }
 });
-
 var ReactwpApp = React.createClass({
     Constant:{
         centerPos:{
             left:0,
-            right:0
+            right:0,
+
         },
-        hPosRange:{
+        hPosA:{
             leftX:[0,0],
             rightX:[0,0],
-            y:[0,0]
+            topY:[0,0]
         },
-        vPosRange:{
+        vPosA:{
             x:[0,0],
             topY:[0,0]
         }
     },
-    arranged:function(center){
-    var imgPosArr = this.state.imgPosArr,
-        Constant = this.Constant,
-        centerPs = Constant.centerPos,
-        hPos = Constant.hPosRange,
-        hPoslX = hPos.leftX,
-        hPosRX = hPos.rightX,
-        hPosY = hPos.y,
-
-        vPos = Constant.vPosRange,
-        vPosX = vPos.x,
-        vPosTopy = vPos.topY,
-        topImgNum = Math.ceil(Math.random() * 2),
-        topIndex = 0,
-        imgTopArr = [],
-        centerImg = imgPosArr.splice(center,1);
-    centerImg[0].pos=centerPs;
-    topIndex = Math.ceil(imgPosArr.length - topImgNum);
-    imgTopArr = imgPosArr.splice(topIndex,topImgNum);
-    console.log(imgTopArr)
-        imgPosArr.splice(topIndex,topImgNum);
-    for(var i = 0,j = imgPosArr.length,k=j / 2; i<j;i++){
-        var posOr = null;
-        if(i<k){
-            posOr = hPoslX;
-        }else {
-            posOr = hPosRX;
-        }
-        imgPosArr[i].pos = {
-            left:arranges(posOr[0],posOr[1]),
-            top:arranges(hPosY[0],hPosY[1]),
-        }
-    }
-        imgTopArr.forEach(function(value,index){
-            imgPosArr[index].pos ={
-                top:arranges(vPosTopy[0],vPosTopy[1]),
-                left:arranges(vPosX[0],vPosX[1]),
+    rerrange:function(center){
+        var imgPosArr = this.state.imgPosArr,
+            Constant = this.Constant,
+            centPos = Constant.centerPos,
+            hPos = Constant.hPosA,
+            hPosLx = hPos.leftX,
+            hPosRx = hPos.rightX,
+            hPosY = hPos.topY,
+            vPos = Constant.vPosA,
+            vPosX = vPos.x,
+            vPosY = vPos.topY,
+            imgTop = [],
+            topNum = Math.ceil(Math.random() * 3),
+            centerNum = Math.ceil(Math.random() * (imgPosArr.length -1)),
+            topIndex = 0;
+            var imgCenter = imgPosArr.splice(centerNum,1);
+        imgCenter[0].pos = centPos;
+            imgTop = imgPosArr.splice(centerNum - topNum,topNum);
+          for(var i=0,j = imgPosArr.length,k = j/2;i<j;i++){
+              var posOr=null;
+              if(i<k){
+                  posOr = hPosLx;
+              }else{
+                  posOr = hPosRx;
+              }
+              imgPosArr[i].pos = {
+                  left:posArannge(posOr[0],posOr[1]),
+                  top:posArannge(hPosY[0],hPosY[1])
+              }
+          };
+           imgTop.forEach(function(value,index){
+               imgTop[index].pos = {
+                   top:posArannge(vPosY[0],vPosY[1]),
+                   left:posArannge(vPosX[0],vPosX[1]),
+                   background:"pink"
+               }
+           });
+        function topSplice(i){
+            imgPosArr.splice(centerNum - topNum,0,imgTop[i])
+        };
+        if(imgTop[1]){
+            for(var i =1;i<topNum;i++){
+                imgPosArr.splice(centerNum - topNum,0,imgTop[i])
             }
-        })
-    if(imgPosArr && imgPosArr[0]){
-        imgPosArr.splice(topIndex,0,imgPosArr[0])
+        }
+        console.log(imgTop)
+        imgPosArr.splice(centerNum - topNum,0,imgTop[0]);
+        imgPosArr.splice(centerNum,0,imgCenter[0]);
+        this.setState(
+            {
+                imgPosArr:imgPosArr
+            }
+        )
     }
-    imgPosArr.splice(center,0,centerImg[0]);
-    this.setState({
-            imgPosArr:imgPosArr
-        }
-    )
-},
+    ,
     getInitialState:function(){
-        return {
-            imgPosArr:[
-                //{
-                //pos:{
-                //    left:0,
-                //    top:0
-                //}
-                //}
-            ]
-        }
-
+       return {
+           imgPosArr:[
+               {
+                   pos:{
+                       //left:0,
+                       //right:0,
+                       //top:0
+                   }
+               }
+           ]
+       }
     },
     componentDidMount:function(){
         var stage = React.findDOMNode(this.refs.stage),
-            stageW =  stage.scrollWidth,
+            stageW = stage.scrollWidth,
             stageH = stage.scrollHeight,
-            halfstgW = Math.ceil(stageW / 2),
-            halfstgH = Math.ceil(stageH / 2),
-            img = React.findDOMNode(this.refs.imgFg0).childNodes[0],
-            imgW = img.clientWidth,
-            imgH = img.clientHeight,
-            halfImgW = Math.ceil(imgW / 2),
-            halfImgH = Math.ceil(imgH / 2);
-        this.Constant.centerPos = {
-            left:halfstgW - halfImgW,
-            top: stageH - imgH
-        };
-        this.Constant.hPosRange.leftX = [-halfImgW,halfstgW - halfImgW*2];
-        this.Constant.hPosRange.rightX = [halfstgW+halfImgW,stageW];
-        this.Constant.hPosRange.y = [-halfImgH,halfstgW - halfImgH*2];
-        this.Constant.vPosRange.x =[ halfstgW - halfImgW  , halfstgW + halfImgW ] ;
-        this.Constant.vPosRange.topY = [-halfImgH / 2,0];
-        this.arranged(0);
+            halfstageW = stageW / 2,
+            halfstageH = stageH / 2,
+
+            imgSize = React.findDOMNode(this.refs.imgFigure0).childNodes[0],
+            imgW  = imgSize.clientWidth,
+            halfimgW = imgW / 2 ,
+            imgH = imgSize.clientHeight,
+            halfimgH = imgH /2 ;
+            this.Constant.centerPos = {
+                left: halfstageW - halfimgW,
+                top: stageH - imgH,
+                background:"black",
+                zIndex:9999
+            };
+            this.Constant.hPosA.leftX = [-halfimgW,halfstageW  - halfimgW * 2.5 ];
+            this.Constant.hPosA.rightX = [halfstageW + halfimgW * 1.5,stageW - halfimgW];
+            this.Constant.hPosA.topY = [-halfimgH,stageH  - halfimgW];
+
+            this.Constant.vPosA.x = [halfstageW - halfimgW, halfstageW + imgW];
+            this.Constant.vPosA.topY = [-halfimgH,stageH - imgH * 1.5];
+            this.rerrange();
     },
-   render:function(){
-       var controllerUnits=[],imgFigures=[];
+    render:function(){
+        var controller=[],imgFigures = [];
+        imgData.forEach(function(value,index){
+            if(!this.state.imgPosArr[index]){
+                this.state.imgPosArr[index]={
+                pos:{
+                    left:0,
+                    top:0
+                }
+                }
+            }
+            imgFigures.push(<ImgFigure data={value} agData={this.state.imgPosArr[index]} ref={"imgFigure" + index}/>)
+        }.bind(this)
+            )
+         return (
+             <div>
+                 <section className="stage" ref="stage">
+                     <section className="img-sec">
+                         {imgFigures}
+                     </section>
+                     <nav className="controller-nav">
 
-       imageDatas.forEach(function(value,index){
-           if(!this.state.imgPosArr[index]){
-               this.state.imgPosArr[index]={
-                   pos: {
-                       left:0,
-                        top:0
-                   }
-               }
-           }
-           imgFigures.push(<ImgFigure data={value} ref={"imgFg" + index} agData={this.state.imgPosArr[index]}/>)
-       }.bind(this));
-       return(
-           <div>
-               <section className="stage" ref="stage">
-                   <section className="img-sec">
-                       {imgFigures}
-                   </section>
-                   <nav className="controller-nav">
-
-                   </nav>
-               </section>
-           </div>
-       )
-   }
+                     </nav>
+                 </section>
+             </div>
+         )
+     }
 })
 React.render(<ReactwpApp />, document.getElementById('content')); // jshint ignore:line
 module.exports = ReactwpApp;
